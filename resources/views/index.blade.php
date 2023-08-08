e34aad69c43c27a90d37b9c6bd14d633cfa96ac8
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-08-08 14:58:31 +0700

    update frontend

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container mx-auto px-4 pt-16">
+  <div class="popular-movies">
+    <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular movies</h2>
+  </div>
+</div>
+@endsection
