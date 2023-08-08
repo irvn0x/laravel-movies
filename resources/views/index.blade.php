29604a7e8fcfb0eed7ba9ebf4c49054b223f4097
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-08-08 21:47:19 +0700

    update frontend add card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,98 @@
 <div class="container mx-auto px-4 pt-16">
   <div class="popular-movies">
     <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular movies</h2>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-10">
+      <div class="mt-8">
+        <a href="">
+          <img src="/img/oppenheimer.jpg" alt="oppenheimer"
+            class="hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+        <div class="mt-2">
+          <a href="" class="text-lg mt-2 hover:text-gray-300">Oppenheimer</a>
+          <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>⭐</span>
+            <span class="ml-1">85%</span>
+            <span class="mx-2">|</span>
+            <span>July 11, 2023</span>
+          </div>
+          <div class="text-gray-400 text-sm">
+            Biography, Drama, History
+          </div>
+        </div>
+      </div>
+      <div class="mt-8">
+        <a href="">
+          <img src="/img/oppenheimer.jpg" alt="oppenheimer"
+            class="hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+        <div class="mt-2">
+          <a href="" class="text-lg mt-2 hover:text-gray-300">Oppenheimer</a>
+          <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>⭐</span>
+            <span class="ml-1">85%</span>
+            <span class="mx-2">|</span>
+            <span>July 11, 2023</span>
+          </div>
+          <div class="text-gray-400 text-sm">
+            Biography, Drama, History
+          </div>
+        </div>
+      </div>
+      <div class="mt-8">
+        <a href="">
+          <img src="/img/oppenheimer.jpg" alt="oppenheimer"
+            class="hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+        <div class="mt-2">
+          <a href="" class="text-lg mt-2 hover:text-gray-300">Oppenheimer</a>
+          <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>⭐</span>
+            <span class="ml-1">85%</span>
+            <span class="mx-2">|</span>
+            <span>July 11, 2023</span>
+          </div>
+          <div class="text-gray-400 text-sm">
+            Biography, Drama, History
+          </div>
+        </div>
+      </div>
+      <div class="mt-8">
+        <a href="">
+          <img src="/img/oppenheimer.jpg" alt="oppenheimer"
+            class="hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+        <div class="mt-2">
+          <a href="" class="text-lg mt-2 hover:text-gray-300">Oppenheimer</a>
+          <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>⭐</span>
+            <span class="ml-1">85%</span>
+            <span class="mx-2">|</span>
+            <span>July 11, 2023</span>
+          </div>
+          <div class="text-gray-400 text-sm">
+            Biography, Drama, History
+          </div>
+        </div>
+      </div>
+      <div class="mt-8">
+        <a href="">
+          <img src="/img/oppenheimer.jpg" alt="oppenheimer"
+            class="hover:opacity-75 transition ease-in-out duration-150">
+        </a>
+        <div class="mt-2">
+          <a href="" class="text-lg mt-2 hover:text-gray-300">Oppenheimer</a>
+          <div class="flex items-center text-gray-400 text-sm mt-1">
+            <span>⭐</span>
+            <span class="ml-1">85%</span>
+            <span class="mx-2">|</span>
+            <span>July 11, 2023</span>
+          </div>
+          <div class="text-gray-400 text-sm">
+            Biography, Drama, History
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 @endsection
