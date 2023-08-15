e961215dc05ed0dccda01a2cc796e7a9501e7481
Irvansyah <ilhamirvansyah.dev@gmail.com>
2023-08-15 14:21:21 +0700

    genre view model

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -15,9 +15,7 @@
         <span>{{ $movie['release_date'] }}</span>
         <span class="mx-2">•</span>
         <span>
-          @foreach ($movie['genres'] as $genre)
-          {{ $genre['name'] }} @if (!$loop->last), @endif
-          @endforeach
+          {{ $movie['genres'] }}
         </span>
       </div>
 
@@ -42,60 +40,57 @@
       <div class="mt-10">
         <h4 class="text-white font-semibold">Featured Crew</h4>
         <div class="flex mt-4">
-          @foreach ($movie['credits']['crew'] as $crew)
-          @if ($loop->index < 5) <div class="mr-8">
+          @foreach ($movie['crew'] as $crew)
+          <div class="mr-8">
             <div>{{ $crew['name'] }}</div>
             <div class="text-sm text-gray-400">{{ $crew['job'] }}</div>
+          </div>
+          @endforeach
         </div>
-        @else
-        @break
+      </div>
+      {{-- End Featured Cast --}}
+
+      {{-- Start Button Trailer --}}
+      <div x-data="{ isOpen: false }">
+        @if (count($movie['videos']['results']) > 0)
+        <div class="mt-10">
+          <button @click="isOpen = true"
+            class="inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" class="w-6 h-6"
+              viewBox="0 0 24 24">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 0 1 0 1.971l-11.54 6.347a1.125 1.125 0 0 1-1.667-.985V5.653z" />
+            </svg>
+            <span class="ml-2">Play Trailer</span>
+          </button>
+        </div>
         @endif
-        @endforeach
-      </div>
-    </div>
-    {{-- End Featured Cast --}}
+        {{-- End Button Trailer --}}
 
-    {{-- Start Button Trailer --}}
-    <div x-data="{ isOpen: false }">
-      @if (count($movie['videos']['results']) > 0)
-      <div class="mt-10">
-        <button @click="isOpen = true"
-          class="inline-flex items-center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1.5" class="w-6 h-6"
-            viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round"
-              d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 0 1 0 1.971l-11.54 6.347a1.125 1.125 0 0 1-1.667-.985V5.653z" />
-          </svg>
-          <span class="ml-2">Play Trailer</span>
-        </button>
-      </div>
-      @endif
-      {{-- End Button Trailer --}}
-
-      {{-- Start Modal Button --}}
-      <div style="background-color: rgba(0, 0, 0, .5);"
-        class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
-        x-show.transition.opacity="isOpen">
-        <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
-          <div class="bg-gray-900 rounded">
-            <div class="flex justify-end pr-4 pt-2">
-              <button @click="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;</button>
-            </div>
-            <div class="modal-body px-8 py-8">
-              <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
-                <iframe width="560" height="315" class="responsive-iframe absolute top-0 left-0 w-full h-full"
-                  src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border: 0;"
-                  allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        {{-- Start Modal Button --}}
+        <div style="background-color: rgba(0, 0, 0, .5);"
+          class="fixed top-0 left-0 w-full h-full flex items-center shadow-lg overflow-y-auto"
+          x-show.transition.opacity="isOpen">
+          <div class="container mx-auto lg:px-32 rounded-lg overflow-y-auto">
+            <div class="bg-gray-900 rounded">
+              <div class="flex justify-end pr-4 pt-2">
+                <button @click="isOpen = false" class="text-3xl leading-none hover:text-gray-300">&times;</button>
+              </div>
+              <div class="modal-body px-8 py-8">
+                <div class="responsive-container overflow-hidden relative" style="padding-top: 56.25%">
+                  <iframe width="560" height="315" class="responsive-iframe absolute top-0 left-0 w-full h-full"
+                    src="https://www.youtube.com/embed/{{ $movie['videos']['results'][0]['key'] }}" style="border: 0;"
+                    allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        {{-- End Modal Button --}}
       </div>
-      {{-- End Modal Button --}}
-    </div>
 
+    </div>
   </div>
-</div>
 </div>
 
 {{-- Start Section Cast --}}
